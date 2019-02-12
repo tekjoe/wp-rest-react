874f68d376d773f9e25e73bb6d4dcc9b31657d37
tekjoe <ramirez.josephh@gmail.com>
2019-02-11 21:10:58 -0600

    Updates

---
+++ precache-manifest.0ef92c1a9ef999a5cc2eada4ce88fbe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "837d68d1594515e1cd59",
+    "url": "/wp-rest-react/static/js/runtime~main.837d68d1.js"
+  },
+  {
+    "revision": "164df9fd8bd04c6ade2d",
+    "url": "/wp-rest-react/static/js/main.164df9fd.chunk.js"
+  },
+  {
+    "revision": "736941608e82605d2aa3",
+    "url": "/wp-rest-react/static/js/2.73694160.chunk.js"
+  },
+  {
+    "revision": "164df9fd8bd04c6ade2d",
+    "url": "/wp-rest-react/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "90d1c622479b43dd805865420c4f05a3",
+    "url": "/wp-rest-react/index.html"
+  }
+];
