126ed2b0584eb990a30fb7840fca606597f63460
tekjoe <ramirez.josephh@gmail.com>
2019-02-11 21:30:38 -0600

    Updates

---
+++ precache-manifest.53da4ba4a81df08157b026ca43545b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "837d68d1594515e1cd59",
+    "url": "/wp-rest-react/static/js/runtime~main.837d68d1.js"
+  },
+  {
+    "revision": "133d46ec68fa4a42c463",
+    "url": "/wp-rest-react/static/js/main.133d46ec.chunk.js"
+  },
+  {
+    "revision": "303f85d8b44b379e645f",
+    "url": "/wp-rest-react/static/js/2.303f85d8.chunk.js"
+  },
+  {
+    "revision": "133d46ec68fa4a42c463",
+    "url": "/wp-rest-react/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1e8bf5f944c6a0b9076826ff456514f9",
+    "url": "/wp-rest-react/index.html"
+  }
+];
